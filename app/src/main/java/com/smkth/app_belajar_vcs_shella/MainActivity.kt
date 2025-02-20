7b406dc636348dc582a4ead8558e82d3bc6c456c
IIISI <shellaliwania@gmail.com>
2025-02-20 12:41:24 +0700

    initial commit
    Signed-off-by: IIISI <shellaliwania@gmail.com>

---
+++ app/src/main/java/com/smkth/app_belajar_vcs_shella/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.smkth.app_belajar_vcs_shella
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.smkth.app_belajar_vcs_shella.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding.btn1.setOnClickListener{
+            Toast.makeText(this,"alloo bub", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
